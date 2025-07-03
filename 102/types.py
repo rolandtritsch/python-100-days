1b6912665ee23d3aaef8bd7306154097162b3ebf
Roland Tritsch <roland@tritsch.email>
2025-07-03 05:59:30 +0100

    102 - types: Initial

---
+++ 102/types.py
@@ -0,0 +1,3 @@
+print(f"list: {list([0, 1, 2])}, access: {list([0, 1, 2])[0]}")
+print(f"tuple: {tuple([0, 1, 2])}, access: {tuple([0, 1, 2])[0]}")
+print(f"dict: {dict([('zero', 0), ('one', 1), ('two', 2)])}, access: {dict([('zero', 0), ('one', 1), ('two', 2)])['zero']}")
