8a4b667d453c4443ebb77147ef4d626d024073db
Roland Tritsch <roland@tritsch.email>
2025-07-03 06:37:26 +0100

    003 - rps: Better var names

--- 003/rps.py
+++ 003/rps.py
@@ -1,7 +1,7 @@
 import random
 
 _rps = ["R", "P", "S"]
-_win = [("R", "S"), ("P", "R"), ("S", "P")]
+_user_wins = [("R", "S"), ("P", "R"), ("S", "P")]
 
 def play():
     user_choice = input("Choose [R]ock, [P]aper, or [S]cissors: ").capitalize()
@@ -10,7 +10,7 @@ def play():
     print(f"Computer chose {computer_choice}.")
     if user_choice == computer_choice:
         print("It's a draw!")
-    elif _win.__contains__((user_choice, computer_choice)):
+    elif _user_wins.__contains__((user_choice, computer_choice)):
         print("You win!")
     else:
         print("You lose!")
