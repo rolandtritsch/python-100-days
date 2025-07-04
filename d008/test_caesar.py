bd7ee64324c9f4e0242a5be305c50806e8011931
Roland Tritsch <roland@tritsch.email>
2025-07-04 07:22:27 +0100

    Make format, check, mypy pass (again).

--- d008/test_caesar.py
+++ d008/test_caesar.py
@@ -1,31 +1,31 @@
 import d008.caesar as caesar
 
 
-def test_chr2idx():
+def test_chr2idx() -> None:
     assert caesar.chr2idx("a") == 0
     assert caesar.chr2idx("z") == 25
 
 
-def test_idx2chr():
+def test_idx2chr() -> None:
     assert caesar.idx2chr(0) == "a"
     assert caesar.idx2chr(25) == "z"
 
 
-def test_shift():
+def test_shift() -> None:
     assert caesar.shift(0, 1) == 1
     assert caesar.shift(len(caesar.ALPHABET) - 1, 1) == 0
     assert caesar.shift(1, -1) == 0
     assert caesar.shift(0, -1) == len(caesar.ALPHABET) - 1
 
 
-def test_encode():
+def test_encode() -> None:
     assert caesar.encode("abc", 1) == "bcd"
     assert caesar.encode("xyz", 2) == "zAB"
     assert caesar.encode("ABC", 3) == "DEF"
     assert caesar.encode("XYZ", 4) == "123"
 
 
-def test_decode():
+def test_decode() -> None:
     assert caesar.decode("bcd", 1) == "abc"
     assert caesar.decode("zAB", 2) == "xyz"
     assert caesar.decode("DEF", 3) == "ABC"
--- d101/dunder.py
+++ d101/dunder.py
@@ -7,13 +7,13 @@ These are special methods that start and end with double underscores.
 import sys
 
 
-def main():
+def main() -> None:
     print(f"annotations: {__annotations__}")
     print(f"builtins: {__builtins__}")
-    print(f"cached: {__cached__}")
+    print(f"cached: {globals().get('__cached__', 'Not defined')}")
     print(f"doc: {__doc__}")
     print(f"file: {__file__}")
-    print(f"loader: {__loader__}")
+    print(f"loader: {globals().get('__loader__', 'Not defined')}")
     print(f"name: {__name__}")
     print(f"package: {__package__}")
     print(f"spec: {__spec__}")
