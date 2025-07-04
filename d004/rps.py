617c617a5f2a78ae47f796ea1bdb0e66b582cdd4
Roland Tritsch <roland@tritsch.email>
2025-07-04 07:34:19 +0100

    d004 - rps: Capitalize (global) constants.

--- d004/rps.py
+++ d004/rps.py
@@ -3,20 +3,20 @@
 import random
 import sys
 
-_rps = ["R", "P", "S"]
-_user_wins = [("R", "S"), ("P", "R"), ("S", "P")]
+RPS = ["R", "P", "S"]
+USER_WINS = [("R", "S"), ("P", "R"), ("S", "P")]
 
 
 def play() -> None:
     """Play a game of Rock Paper Scissors"""
 
     user_choice = input("Choose [R]ock, [P]aper, or [S]cissors: ").capitalize()
-    computer_choice = _rps[random.randint(0, len(_rps) - 1)]
+    computer_choice = RPS[random.randint(0, len(RPS) - 1)]
     print(f"You chose {user_choice}.")
     print(f"Computer chose {computer_choice}.")
     if user_choice == computer_choice:
         print("It's a draw!")
-    elif (user_choice, computer_choice) in _user_wins:
+    elif (user_choice, computer_choice) in USER_WINS:
         print("You win!")
     else:
         print("You lose!")
